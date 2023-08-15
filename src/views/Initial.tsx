73fbbd3b600588d5c12bbf9544f1ed791ed71991
Suka <mateodefalco1@gmail.com>
2023-08-15 11:48:44 -0300

    Playing feed

--- src/views/Initial.tsx
+++ src/views/Initial.tsx
@@ -6,22 +6,26 @@ export default function Initial() {
 
   function getRandomColors() {
     const colorIndexes: number[] = [];
+
     while (colorIndexes.length < 2) {
       const randomIndex = Math.floor(Math.random() * colorsData.length);
+
       if (!colorIndexes.includes(randomIndex)) {
         colorIndexes.push(randomIndex);
       }
     }
     const color1 = colorsData[colorIndexes[0]];
     const color2 = colorsData[colorIndexes[1]];
+
     return [color1, color2];
   }
 
   function handlePlay() {
+    const [color1, color2] = getRandomColors();
+
     setStatus("playing");
     setTime(60);
     setScore(0);
-    const [color1, color2] = getRandomColors();
     setColor([color1, color2]);
   }
 
--- src/views/Playing.tsx
+++ src/views/Playing.tsx
@@ -1,24 +1,39 @@
+import { useEffect } from "react";
 import { useGameContext } from "../GameContext";
 
 export default function Playing() {
   const { status, setStatus, time, setTime, score, setScore, color } =
     useGameContext();
-  console.log("Suka ~ file: Playing.tsx:5 ~ color:", color);
+
+  // Generar un índice aleatorio para seleccionar uno de los colores disponibles
+  const randomIndex = Math.floor(Math.random() * color.length);
+  const randomColor = color[randomIndex];
+
 
   return (
-    <div className="container">
+    <>
       {color.length !== 0 && (
-        <div style={{ display: "flex", flexDirection: "row", gap: 8 }}>
-          <button style={{ width: 128, height: 128 }}>
-            {color[0].name.toUpperCase()}
-          </button>
-          <button style={{ width: 128, height: 128 }}>
-            {color[1].name.toUpperCase()}
-          </button>
+        <div className="container">
+          {/* HEADER */}
+          <div style={{ display: "flex", flexDirection: "row", gap: 8 }}>
+            <div>{time}</div>
+            <div>{score}</div>
+          </div>
+          {/* COLOR NAME */}
+          <div style={{ display: "flex", justifyContent: "center", gap: 8 }}>
+            <p style={{ fontSize: 40 }}>{randomColor.name.toUpperCase()}</p>
+          </div>
+          {/* COLOR OPTIONS */}
+          <div style={{ display: "flex", flexDirection: "row", gap: 8 }}>
+            <button style={{ width: 128, height: 128 }}>
+              {color[0].name.toUpperCase()}
+            </button>
+            <button style={{ width: 128, height: 128 }}>
+              {color[1].name.toUpperCase()}
+            </button>
+          </div>
         </div>
       )}
-      <div>{time}</div>
-      <div>{score}</div>
-    </div>
+    </>
   );
 }
